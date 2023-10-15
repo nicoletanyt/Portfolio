4c2c7821ea7df5c4124886d87903676dd3d747f8
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-10-15 23:07:51 +0800

    front page done

--- root/src/App.js
+++ root/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <NavBar />
+      <div id="intro-page">
+        <div id="name-img-wrapper">
+          <div id="name-wrapper">
+            <h1>Hi, my name is </h1>
+            <h1 id="name">Nicole.</h1>
+            <p>
+              I'm a 15 year-old student who likes to listen to music, code, and
+              do math!
+            </p>
+          </div>
+          <img id="name-img" />
+        </div>
+        <div id="scroll-wrapper">
+          <p className="scroll-text">SCROLL</p>
+          <hr className="vertical-line" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ root/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div id="navbar-container">
+      <div>{/* favicon */}</div>
+      <h2>PROJECTS</h2>
+      <h2>AWARDS</h2>
+    </div>
+  );
+}
