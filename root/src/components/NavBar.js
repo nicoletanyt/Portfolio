a25e95fac0f4cf9e67b8d54fc97a47e5ab30327e
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-10-16 11:25:09 +0800

    Added btns to socials

--- root/src/components/NavBar.js
+++ root/src/components/NavBar.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { VscGithub } from "react-icons/vsc";
+import { SiCodewars, SiLinkedin } from "react-icons/si";
 
 export default function NavBar() {
   return (
     <div id="navbar-container">
-      <div>{/* favicon */}</div>
-      <h3>PROJECTS</h3>
-      <h3>AWARDS</h3>
+      <div>
+        <div>{/* favicon */}</div>
+        <h3>PROJECTS</h3>
+        <h3>AWARDS</h3>
+      </div>
+      <div id="socials-wrapper">
+        <a href="https://github.com/nicoletanyt">
+          <VscGithub className="sidebar-icons" />
+        </a>
+        <a href="https://www.codewars.com/users/nicoletanyt">
+          <SiCodewars className="sidebar-icons" />
+        </a>
+        <a href="https://www.linkedin.com/in/nicole-tan-a1a360266/">
+          <SiLinkedin className="sidebar-icons" />
+        </a>
+      </div>
     </div>
   );
 }
