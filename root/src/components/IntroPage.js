72bddfe887e96456aee80b6153a606828ddce383
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-06-01 13:27:04 +0800

    added animation for ascii art

--- root/src/components/IntroPage.js
+++ root/src/components/IntroPage.js
@@ -1,27 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function IntroPage() {
-  return (
-    <div id="intro-page">
-      <div id="name-img-wrapper">
-        <div id="ascii-art">
-            <pre>
-              {`
-      {   }
-      }_{ __{
-    .-{   }   }-.
+
+  const [frameIndex, setFrameIndex] = useState(0)
+
+  const frames = [
+    `
+     {   }
+      }_{ _{
+    .-{  }   }-.
   (   }     {   )
   | \`-.._____..-'|
   |             ;--.
   |            (__  \\
   |             | )  )
-  |    nicole   |/  /
+  |    Nicole   |/  /
   |             /  /   
   |            (  /
   \\             y'
-    \`-.._____..-'
-              `}
-            </pre>
+    \`-.._____..-'`,
+    `
+        {   }
+       }_{ _{
+    .-{  }   }-.
+  (   }     {   )
+  | \`-.._____..-'|
+  |             ;--.
+  |            (__  \\
+  |             | )  )
+  |    nIcole   |/  /
+  |             /  /   
+  |            (  /
+  \\             y'
+    \`-.._____..-'`,
+    `
+          {  }
+        }_{_ {
+    .-{  }   }-.
+  (   }     {   )
+  | \`-.._____..-'|
+  |             ;--.
+  |            (__  \\
+  |             | )  )
+  |    niCole   |/  /
+  |             /  /   
+  |            (  /
+  \\             y'
+    \`-.._____..-'`,
+    `
+     {   }
+      }_{ _{
+    .-{  }   }-.
+  (   }     {   )
+  | \`-.._____..-'|
+  |             ;--.
+  |            (__  \\
+  |             | )  )
+  |    nicOle   |/  /
+  |             /  /   
+  |            (  /
+  \\             y'
+    \`-.._____..-'`,
+    `
+        {   }
+       }_{ _{
+    .-{  }   }-.
+  (   }     {   )
+  | \`-.._____..-'|
+  |             ;--.
+  |            (__  \\
+  |             | )  )
+  |    nicoLe   |/  /
+  |             /  /   
+  |            (  /
+  \\             y'
+    \`-.._____..-'`,
+    `
+          {  }
+        }_{_ {
+    .-{  }   }-.
+  (   }     {   )
+  | \`-.._____..-'|
+  |             ;--.
+  |            (__  \\
+  |             | )  )
+  |    nicolE   |/  /
+  |             /  /   
+  |            (  /
+  \\             y'
+    \`-.._____..-'`,
+  ]
+
+  // Changing of frames for animation
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFrameIndex(prevFrameIndex => prevFrameIndex + 1);
+    }, 500);
+    
+    return () => clearInterval(intervalId);
+  }, [])
+
+  useEffect(() => {
+    console.log(frameIndex)
+    document.getElementById("ascii-art").innerHTML = frames[frameIndex % frames.length]
+  }, [frameIndex])
+
+  return (
+    <div id="intro-page">
+      <div id="name-img-wrapper">
+        <div>
+            <pre id="ascii-art">{frames[0]}</pre>
           </div>
         <div id="name-wrapper">
           <h1>Hi, I'm Nicole Tan</h1>
